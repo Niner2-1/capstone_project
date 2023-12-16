f4608663b789085b58673a50c94f48d98d51cd99
John Roiden Abulencia <48873644+Niner2-1@users.noreply.github.com>
2023-12-16 16:01:04 +0800

    Add files via upload

---
+++ Capstone_Project/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang = "eng">
+	<head>
+		<title>LAFUENTE MEDICAL CLINIC Patient Record Management Information System</title>
+		<meta charset = "utf-8" />
+		<link rel = "shortcut icon" href = "images/logo.png">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel = "stylesheet" type = "text/css" href = "css\bootstrap.css" />
+		<link rel = "stylesheet" type = "text/css" href = "css\index.css" />
+		
+	</head>
+<body>
+	<header>
+	<div class = "navbar navbar-default">
+		<img src = "images/logo.png" style = "float:left;" height = "55px" /><label class = "navbar-brand">LAFUENTE MEDICAL CLINIC Patient Record Management Information System - Padre Burgos</label>
+	</div>
+	</header>
+
+
+
+
+	<div class="div_container1">
+
+	<div class="card">
+		<div class="card__content">1</div>
+	</div>
+    <div class="card">
+		<div class="card__content">2</div>
+	</div>
+	<div class="card">
+		<div class="card__content">3</div>
+	</div>
+
+
+	</div>
+
+
+
+
+
+
+
+	
+	<div class = "footer">
+		<label class = "footer-title"> FOOTER SAMPLE</label>
+	</div>
+</body>
+
+
+</html>
